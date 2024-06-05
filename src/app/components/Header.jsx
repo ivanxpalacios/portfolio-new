7803e3397819b910631137f8992c14e17c39f0a4
Iván Palacios Martínez <ivan@blife.mx>
2024-06-04 21:32:58 -0600

    Volvimos sticky el header y función al logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,12 +1,28 @@
 "use client";
 import Image from "next/image"
 import Sidebar from "./Sidebar"
+import { useRouter, usePathname } from "next/navigation";
 
 export default function Header() {
+
+  const router = useRouter();
+
+  const path = usePathname();
+
+  const handleClickLogo = () => {
+    if (path === "/") {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      router.push("/");
+    }
+  }
+
   return (
-    <header className="p-5 max-w-[1200px] mx-auto">
+    <header className="p-5 max-w-[1200px] mx-auto sticky top-0 bg-blackprimary z-[10]">
       <div className="flex items-center justify-between">
-        <button>
+        <button
+          onClick={handleClickLogo}
+        >
           <Image
             src="/images/logo-horizontal.svg"
             alt="logo"
