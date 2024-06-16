b12590028983c9dce9cf1e26eefd1359a67d8be5
Iván Palacios Martínez <ivan@blife.mx>
2024-06-16 15:22:24 -0600

    Terminando con las animaciones

--- src/app/page.js
+++ src/app/page.js
@@ -6,29 +6,29 @@ import { experiencia } from "./data/experiencia";
 import SwiperSkillset from "./components/swiper-skillset/SwiperSkillset";
 import SwiperProjectsCards from "./components/swiper-projects-cards/SwiperProjectsCards";
 import { contactIcons } from "./data/contactIcons";
+import ScrollAnimation from 'react-animate-on-scroll';
 
 export default function Home() {
   return (
     <main>
-      <section  className="mt-14 lg:mt-0 lg:flex items-center justify-between banner-home-desktop max-w-[1300px] mx-auto" id="home">
+      <section  className="mt-14 lg:mt-0 lg:flex items-center justify-between banner-home-desktop max-w-[1300px] mx-auto fadeInUp" style={{animationDelay: "0.3s"}} id="home">
         <div
           className="px-5 lg:max-w-[470px] xl:max-w-[500px]"
         >
-          <h1 className="text-white text-[28px] lg:text-[50px] font-bold leading-[34px] lg:leading-[56px]">
-            Hi, I am <br /> Iván Palacios
-          </h1>
-
+            <h1 className="fadeInUp text-white text-[28px] lg:text-[50px] font-bold leading-[34px] lg:leading-[56px]" style={{animationDelay: "0.3s"}}>
+              Hi, I am <br /> Iván Palacios
+            </h1>
           <h2
-            className="mt-6 text-[#828282] font-[600] text-[18px] lg:text-[26px]"
+            className="fadeInUp mt-6 text-[#828282] font-[600] text-[18px] lg:text-[26px]" style={{animationDelay: "0.6s"}}
           >
             Frontend Engineer
           </h2>
 
-          <p className="mt-2 text-white font-[200] text-[18px] lg:text-[20px]">
+          <p className="fadeInUp mt-2 text-white font-[200] text-[18px] lg:text-[20px]" style={{animationDelay: "0.6s"}}>
             Building pixel-perfect, engaging and accessible frontend products with best user experiences.
           </p>
 
-          <div className="mt-6 flex items-center gap-6">
+          <div className="fadeInUp mt-6 flex items-center gap-6" style={{animationDelay: "0.6s"}}>
             <button
               className="bg-primary h-10 rounded-[10px] w-full text-white text-[14px] lg:text-[18px] font-[300] lg:max-w-[190px]"
             >
@@ -49,112 +49,135 @@ export default function Home() {
           <Image
             src="/images/foto-personal.png"
             alt="foto-personal"
-            className="w-[75%] md:max-w-[400px] h-auto object-cover lg:hidden"
+            className="w-[75%] md:max-w-[400px] h-auto object-cover lg:hidden fadeInUp"
             width={300}
             height={600}
             priority
+            style={{animationDelay: "1s"}}
           />
         </div>
 
-        <div className="px-5">
+        <div className="fadeInUp px-5" style={{animationDelay: "1.2s"}}>
           <hr className="bg-primary w-full h-[10px] rounded-[20px] border-0" />
         </div>
       </section>
 
       {/* Sección About me */}
       <section className="px-5 mt-20 lg:mt-28 max-w-[1300px] mx-auto" id="about-me">
-        <h1 className="text-white text-[36px] lg:text-[50px] font-bold leading-[42px] text-center">
-          <span className="text-primary">/</span>About me
-        </h1>
+        <ScrollAnimation animateIn="fadeInUp" animateOnce duration={0.7} className="lg:hidden">
+          <h1 className="text-white text-[36px] lg:text-[50px] font-bold leading-[42px] text-center">
+            <span className="text-primary">/</span>About me
+          </h1>
 
-        <Image
-          src="/images/svg-about-me.svg"
-          alt="about-me"
-          className="w-full max-w-[500px] mx-auto h-auto object-cover mt-6 lg:hidden"
-          width={300}
-          height={600}
-        />
+          <Image
+            src="/images/svg-about-me.svg"
+            alt="about-me"
+            className="w-full max-w-[500px] mx-auto h-auto object-cover mt-6 lg:hidden"
+            width={300}
+            height={600}
+          />
+        </ScrollAnimation>
 
-        <Image
-          src="/images/svg-about-me-desktop.svg"
-          alt="about-me"
-          className="w-full max-w-[750px] mx-auto h-auto object-cover mt-6 hidden lg:block"
-          width={600}
-          height={600}
-        />
+        <div className="fadeInUp hidden lg:block" style={{animationDelay: "0.6s"}}>
+          <h1 className="text-white text-[36px] lg:text-[50px] font-bold leading-[42px] text-center">
+            <span className="text-primary">/</span>About me
+          </h1>
+
+          <Image
+            src="/images/svg-about-me-desktop.svg"
+            alt="about-me"
+            className="w-full max-w-[750px] mx-auto h-auto object-cover mt-6 hidden lg:block"
+            width={600}
+            height={600}
+          />
+        </div>
+
 
         {/* What I do */}
         <div className="mt-14 lg:mt-20">
-          <div className="lg:flex justify-between items-start">
-            <h2
-              className="text-white text-[28px] lg:text-[44px]"
-            >
-              <span className="text-primary font-bold">#</span>what I do
-            </h2>
+          <ScrollAnimation animateIn="fadeInUp" animateOnce duration={0.7}>
+            <div className="lg:flex justify-between items-start">
+              <h2
+                className="text-white text-[28px] lg:text-[44px]"
+              >
+                <span className="text-primary font-bold">#</span>what I do
+              </h2>
 
-            <p className="mt-6 text-white leading-[22px] lg:text-[20px] lg:leading-[26px] font-[300] border-l border-primary px-6 lg:max-w-[660px]">
-              Currently working as the <span className="font-[600]">Front-End Development Team Lead</span>, I create interfaces for e-commerce projects visited by thousands of clients. I coordinate the activities of various developers and handle the application process and hiring of new team members.
-            </p>
+              <p className="mt-6 text-white leading-[22px] lg:text-[20px] lg:leading-[26px] font-[300] border-l border-primary px-6 lg:max-w-[660px]">
+                Currently working as the <span className="font-[600]">Front-End Development Team Lead</span>, I create interfaces for e-commerce projects visited by thousands of clients. I coordinate the activities of various developers and handle the application process and hiring of new team members.
+              </p>
 
-          </div>
+            </div>
+          </ScrollAnimation>
 
-          <div className="mt-14 lg:mt-20">
-            <CardsWhatIDo />
-          </div>
+          <ScrollAnimation animateIn="fadeInUp" animateOnce duration={0.7}>
+            <div className="mt-14 lg:mt-20">
+              <CardsWhatIDo />
+            </div>
+          </ScrollAnimation>
+
         </div>
       </section>
 
       {/* Sección Experience */}
       <section className="px-5 mt-20 lg:mt-28 max-w-[1300px] mx-auto" id="experience">
 
-        <h1 className="text-white text-[36px] lg:text-[50px]  font-bold leading-[42px] text-center">
-          <span className="text-primary">/</span>Experience
-        </h1>
+        <ScrollAnimation animateIn="fadeInUp" animateOnce duration={0.7} >
+          <h1 className="text-white text-[36px] lg:text-[50px]  font-bold leading-[42px] text-center">
+            <span className="text-primary">/</span>Experience
+          </h1>
 
-        <p className="mt-6 lg:mt-10 mb-2 text-white leading-[22px] lg:text-[20px] lg:leading-[26px]  font-[300] border-l border-primary px-6 lg:max-w-[752px] lg:mx-auto">
-          Specialized in converting designs into responsive interfaces with a strong focus on UX/UI, efficient debugging, and coordinating with fellow programmers. My experience ensures high-quality, user-friendly web experiences.
-        </p>
+          <p className="mt-6 lg:mt-10 mb-2 text-white leading-[22px] lg:text-[20px] lg:leading-[26px]  font-[300] border-l border-primary px-6 lg:max-w-[752px] lg:mx-auto">
+            Specialized in converting designs into responsive interfaces with a strong focus on UX/UI, efficient debugging, and coordinating with fellow programmers. My experience ensures high-quality, user-friendly web experiences.
+          </p>
+        </ScrollAnimation>
 
-        <div className="lg:mt-10">
-          {experiencia.map((exp, index) => (
-            <ExperienceDisclosure
-              key={index}
-              exp={exp}
-            />
-          ))}
-        </div>
-
-        <div className="mt-14 lg:mt-20">
-          <h2
-            className="text-white text-[28px] lg:text-[44px]"
-          >
-            <span className="text-primary font-bold">#</span>My Skillset
-          </h2>
-
-          <div className="mt-4 lg:mt-10  bg_blur_skills">
-            <SwiperSkillset />
+        <ScrollAnimation animateIn="fadeInUp" animateOnce duration={0.7} >
+          <div className="lg:mt-10">
+            {experiencia.map((exp, index) => (
+              <ExperienceDisclosure
+                key={index}
+                exp={exp}
+              />
+            ))}
           </div>
-        </div>
+        </ScrollAnimation>
+
+          <div className="mt-14 lg:mt-20">
+            <ScrollAnimation animateIn="fadeInUp" animateOnce duration={0.7}>
+              <h2
+                className="text-white text-[28px] lg:text-[44px]"
+              >
+                <span className="text-primary font-bold">#</span>My Skillset
+              </h2>
+            </ScrollAnimation>
+            
+            <div className="mt-4 lg:mt-10  bg_blur_skills">
+              <SwiperSkillset />
+            </div>
+          </div>
       </section>
 
       {/* Sección Projects */}
       <section className="px-5 mt-20 lg:mt-28 max-w-[1300px] mx-auto" id="projects">
-        <h1 className="text-white text-[36px] lg:text-[50px] font-bold leading-[42px] text-center">
-          <span className="text-primary">/</span>Projects
-        </h1>
+        <ScrollAnimation animateIn="fadeInUp" animateOnce duration={0.7}>
+          <h1 className="text-white text-[36px] lg:text-[50px] font-bold leading-[42px] text-center">
+            <span className="text-primary">/</span>Projects
+          </h1>
 
-        <div className="mt-10 lg:mt-14 lg:flex justify-between">
-          <h2
-            className="text-white text-[28px] lg:text-[44px] leading-[34px] lg:leading-[50px]"
-          >
-            <span className="text-primary font-bold">#</span>Look at my <br /> <span className="text-primary">projects</span>
-          </h2>
+          <div className="mt-10 lg:mt-14 lg:flex justify-between">
+            <h2
+              className="text-white text-[28px] lg:text-[44px] leading-[34px] lg:leading-[50px]"
+            >
+              <span className="text-primary font-bold">#</span>Look at my <br /> <span className="text-primary">projects</span>
+            </h2>
 
-          <p className="mt-6 text-white leading-[22px] lg:text-[20px] lg:leading-[26px] font-[300] border-l border-primary px-6 lg:max-w-[660px] lg:flex items-center">
-          I am part of the process of creating and maintaining multiple e-commerce projects such as the ones shown below:
-          </p>
+            <p className="mt-6 text-white leading-[22px] lg:text-[20px] lg:leading-[26px] font-[300] border-l border-primary px-6 lg:max-w-[660px] lg:flex items-center">
+            I am part of the process of creating and maintaining multiple e-commerce projects such as the ones shown below:
+            </p>
 
-        </div>
+          </div>
+        </ScrollAnimation>
 
         <div className="mt-10 lg:mt-14">
           <SwiperProjectsCards />
@@ -164,69 +187,73 @@ export default function Home() {
       {/* Sección Contact */}
       <section  className="px-5 my-20 lg:my-28 max-w-[1300px] mx-auto" id="contact">
         <div className="xl:flex items-center justify-between">
-          <div>
-            {/* Div Let's work together */}
-            <div
-              className="border-b pb-2 w-fit flex items-end gap-4"
-            >
-              <h2
-                className="text-white text-[28px] lg:text-[44px] leading-[34px] lg:leading-[50px]"
-              >
-                <span className="text-primary font-bold">#</span>Let's work<br /> <span className="text-primary">together</span>
-              </h2>
-
-              <svg width="22" height="22" viewBox="0 0 14 14" fill="#FFF" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1.65428 13.9146L0.946533 13.2068L12.2388 1.91455H4.94653V0.914551H13.9465V9.91455H12.9465V2.6223L1.65428 13.9146Z" fill="#FFF"/>
-              </svg>
-
-            </div>
-
-            <div className="flex justify-between items-center xl:flex-col xl:items-start">
-              <p
-                className="mt-6 font-bold text-[18px] lg:text-[24px] uppercase text-white"
-                >
-                Contact<span className="ms-1">me</span>
-              </p>
-
-              <ul className="mt-5 flex items-center justify-center gap-2">
-                {contactIcons.map((item, index) => (
-                  <li key={index}>
-                    <button
-                      className="fill-[#535353] hover:fill-white transition-[fill]"
-                      onClick={() => window.open(item.link)}
-                    >
-                      {item.icon}
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-
-
-          <div className="xl:flex items-end justify-between">
-            <Image
-              src={"/images/contact-cards.webp"}
-              alt="contact-cards"
-              width={600}
-              height={600}
-              className="w-full h-auto mt-10 max-w-[500px] mx-auto"        
-            />
-
+          <ScrollAnimation animateIn="fadeInUp" animateOnce duration={0.7}>
             <div>
-              <p className="text-[22px] mb-4 text-primary font-bold text-center"> 
-                SCAN ME!
-              </p>
+              {/* Div Let's work together */}
+              <div
+                className="border-b pb-2 w-fit flex items-end gap-4"
+              >
+                <h2
+                  className="text-white text-[28px] lg:text-[44px] leading-[34px] lg:leading-[50px]"
+                >
+                  <span className="text-primary font-bold">#</span>Let's work<br /> <span className="text-primary">together</span>
+                </h2>
 
+                <svg width="22" height="22" viewBox="0 0 14 14" fill="#FFF" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1.65428 13.9146L0.946533 13.2068L12.2388 1.91455H4.94653V0.914551H13.9465V9.91455H12.9465V2.6223L1.65428 13.9146Z" fill="#FFF"/>
+                </svg>
+
+              </div>
+
+              <div className="flex justify-between items-center xl:flex-col xl:items-start">
+                <p
+                  className="mt-6 font-bold text-[18px] lg:text-[24px] uppercase text-white"
+                  >
+                  Contact<span className="ms-1">me</span>
+                </p>
+
+                <ul className="mt-5 flex items-center justify-center gap-2">
+                  {contactIcons.map((item, index) => (
+                    <li key={index}>
+                      <button
+                        className="fill-[#535353] hover:fill-white transition-[fill]"
+                        onClick={() => window.open(item.link)}
+                      >
+                        {item.icon}
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </ScrollAnimation>
+
+          <ScrollAnimation animateIn="fadeInUp" animateOnce duration={0.7}>
+            <div className="xl:flex items-end justify-between">
               <Image
-                src={"/images/qr-code.svg"}
-                alt="qr code"
+                src={"/images/contact-cards.webp"}
+                alt="contact-cards"
                 width={600}
                 height={600}
-                className="w-full max-w-[200px] mx-auto h-auto"        
+                className="w-full h-auto mt-10 max-w-[500px] mx-auto"        
+                quality={100}
               />
+
+              <div>
+                <p className="text-[22px] mb-4 text-primary font-bold text-center"> 
+                  SCAN ME!
+                </p>
+
+                <Image
+                  src={"/images/qr-code.svg"}
+                  alt="qr code"
+                  width={600}
+                  height={600}
+                  className="w-full max-w-[200px] mx-auto h-auto"        
+                />
+              </div>
             </div>
-          </div>
+          </ScrollAnimation>
         </div>
 
 
